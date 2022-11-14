161f8e21cba6fa5f656d99f6d4a00f292732ca77
ebabic1 <ebabic1@etf.unsa.ba>
2022-11-14 16:27:28 +0100

    Modified algorithm doesn't push left braces to stack + test decoupling

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -1,26 +1,30 @@
 package ba.unsa.etf.rpr;
 
 import java.util.Stack;
-
+/** Parses a fully parenthesized arithmetic expression. A fully parenthesized arithmetic expression is an arithmetic
+ *          expression where every operator
+ *          and its arguments are contained in parentheses, as seen in following: ( 2 + 3 ) or ( 1 + ( ( 2 + 3 ) ∗ ( 4 ∗ 5 ) ) ) - note the spaces in between */
 public class ExpressionEvaluator {
-    private Stack<Double> operandStack;
-    private Stack<String> operatorStack;
-
-    public ExpressionEvaluator() {
-            operatorStack = new Stack<String>();
-            operandStack = new Stack<Double>();
-    }
+    private final Stack<Double> operandStack;
+    private final Stack<String> operatorStack;
+    private int lBrace = 0;
     /** Class constructor */
+    public ExpressionEvaluator() {
+            operatorStack = new Stack<>();
+            operandStack = new Stack<>();
+    }
 
-    /** Parses an arithmetic expression using Dijkstra algorithm
+
+    /** main evaluation method
      * @param s fully parenthesized arithmetic expression
+     *
      * @return result
      * @throws RuntimeException in case of missing operands  */
     public Double evaluate(String s) {
-        Double result;
-        String el = null;
+        boolean flag = false;
         for(String element : s.trim().split(" ")) {
-            if(isOperator(element) || element.equals("(")) operatorStack.push(element);
+            if(isOperator(element)) {operatorStack.push(element);}
+            else if(element.equals("(")) {lBrace = lBrace+1; flag = true;}
             else if(element.equals(")"))
             {
                 if(operandStack.isEmpty() || operatorStack.isEmpty()) {
@@ -29,37 +33,35 @@ public class ExpressionEvaluator {
                 String operator = operatorStack.pop();
                 Double value = operandStack.pop();
                 this.applyOperator(operator,value);
+                lBrace--;
             }
-            else if (!operatorStack.isEmpty()) try{
+            else try{
                     operandStack.push(Double.parseDouble(element));
                 } catch (Exception e) {
-                    throw new RuntimeException("Invalid expression");
+                    throw new RuntimeException("Invalid symbols in expression");
                 }
         }
-        if (operandStack.size() == 1) result = operandStack.pop();
+        if (operandStack.size() == 1 && lBrace == 0 && flag) return operandStack.pop();
         else {
             throw new RuntimeException("Invalid expression");
         }
-        return result;
     }
     /** Method for applying given operator to values
-     * @param operator
-     * @return  void
+     * @param operator, value
      * @throws RuntimeException in case of missing operands*/
     private void applyOperator(String operator, Double value) throws RuntimeException{
-        if(operator.equals("(")){
-            operandStack.push(value);
-            return;
-        }
+        /* in case of number surrounded in braces eg. ( ... ( 1 ) ... )*/
+
         if(operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
             if(operandStack.isEmpty()) throw new RuntimeException("Invalid expression - missing operand");
         }
-        if(operator.equals("+")) operandStack.push(operandStack.pop()+value);
-        else if(operator.equals("-")) operandStack.push(operandStack.pop()-value);
-        else if(operator.equals("*")) operandStack.push(operandStack.pop()*value);
-        else if(operator.equals("/")) operandStack.push(operandStack.pop()/value);
-        else if(operator.equals("sqrt")) operandStack.push(Math.sqrt(value));
-        if(operatorStack.empty() || !operatorStack.pop().equals("(")) throw new RuntimeException("Invalid expression - right bracket left open");
+        switch (operator) {
+            case "+" -> operandStack.push(operandStack.pop() + value);
+            case "-" -> operandStack.push(operandStack.pop() - value);
+            case "*" -> operandStack.push(operandStack.pop() * value);
+            case "/" -> operandStack.push(operandStack.pop() / value);
+            case "sqrt" -> operandStack.push(Math.sqrt(value));
+        }
     }
     /* Method for checking if given string represents an operator
      * @param string
--- src/test/java/ExpressionEvaluatorTest.java
+++ src/test/java/ExpressionEvaluatorTest.java
@@ -1,5 +1,4 @@
 import ba.unsa.etf.rpr.ExpressionEvaluator;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,28 +7,36 @@ public class ExpressionEvaluatorTest {
     public ExpressionEvaluator evaluator = new ExpressionEvaluator();
 
     @Test
-    public void testInvalidExpression() {
+    public void testInvalidExpression1() {
         assertThrows(RuntimeException.class,()->evaluator.evaluate("( 1 + ( 2 )"));
-        assertThrows(RuntimeException.class,()->evaluator.evaluate("( 1 +  2 ) +"));
-        assertThrows(RuntimeException.class,()->evaluator.evaluate("( +  2 )"));
-        assertThrows(RuntimeException.class,()->evaluator.evaluate("sqrt 1 )"));
-        assertThrows(RuntimeException.class,()->evaluator.evaluate(" + 1 "));
     }
     @Test
-    /*Test iz postavke*/
-    public void testEvaluator1() {
-        assertEquals(101,evaluator.evaluate("( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )"));
-    }
-    @Test
-    public void testEvaluator2() {
-        assertEquals(15.070522012751594,evaluator.evaluate("( ( sqrt 1 ) + ( ( ( sqrt 2 ) + ( sqrt 3 ) ) * ( ( sqrt 4 ) * ( sqrt 5 ) ) ) )"));
-    }
-    @Test
-    public void testEvaluator3(){
-        assertEquals(42,(evaluator.evaluate(" ( ( ( ( 42 ) ) ) )")));
-    }
-    @Test
-    public void testEvaluator4(){
-        assertEquals(1.189207115002721,evaluator.evaluate("( sqrt ( sqrt 2 ) )"));
+        public void testInvalidExpression2() {
+            assertThrows(RuntimeException.class,()->evaluator.evaluate("( 1 +  2 ) +"));
+        }
+        @Test
+        public void testInvalidExpression3() {
+            assertThrows(RuntimeException.class,()->evaluator.evaluate("( +  2 )"));
+        }
+        @Test
+        public void testInvalidExpression4() {
+            assertThrows(RuntimeException.class,()->evaluator.evaluate("sqrt 1 )"));
+        }
+        @Test
+        public void testInvalidExpression5() {
+            assertThrows(RuntimeException.class,()->evaluator.evaluate(" + 1 "));
+        }
+
+        @Test
+        public void testEvaluator1() {
+            assertEquals(101,evaluator.evaluate("( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )"));
+        }
+        @Test
+        public void testEvaluator2() {
+            assertEquals(15.070522012751594,evaluator.evaluate("( ( sqrt 1 ) + ( ( ( sqrt 2 ) + ( sqrt 3 ) ) * ( ( sqrt 4 ) * ( sqrt 5 ) ) ) )"));
+        }
+        @Test
+        public void testEvaluator4(){
+            assertEquals(1.189207115002721,evaluator.evaluate("( sqrt ( sqrt 2 ) )"));
     }
 }
