84e42880c72d57ef4b7a4634bd389a33567fb7dc
ebabic1 <ebabic1@etf.unsa.ba>
2022-11-18 19:36:13 +0100

    main changes

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -3,14 +3,17 @@ package ba.unsa.etf.rpr;
 public class App {
     public static void main(String[] args) {
         String inputString = "";
-        for(int i = 0; i < args.length; i++){
-            inputString += args[i];
+        for (String arg : args) {
+            inputString += arg;
+        }
+        if(inputString.isEmpty()) {
+            throw new RuntimeException("Unijeli ste prazan string!");
         }
         ExpressionEvaluator evaluator = new ExpressionEvaluator();
         try{
             System.out.println(evaluator.evaluate(inputString));
         }catch (RuntimeException exception){
-            System.out.println(exception);
+            System.out.println(exception.getMessage());
         }
 
     }
